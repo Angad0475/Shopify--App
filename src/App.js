bf43b1e2c3f3302012b7e122e57725d61714a991
Angad <angad04755@gmail.com>
2024-05-10 16:03:45 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import {Routes,Route} from 'react-router-dom';
+import {Routes,Route, BrowserRouter} from 'react-router-dom';
 import Navbar from "./components/Navbar/Navbar";
  import Shop from './pages/Shop';
  import ShopCategory from './pages/ShopCategory';
  import Cart from './pages/Cart';
  import Product from './pages/Product';
  import LoginSignup from './pages/LoginSignup';
+import Footer from './components/Footer/Footer';
 
 
 function App(){
     return(
         <div>
+                <BrowserRouter>
                 <Navbar/>
                 <Routes>
                     <Route path='/' element={<Shop/>}></Route>
@@ -24,6 +26,8 @@ function App(){
                     <Route path='/cart' element={<Cart/>}/>
                     <Route path='/login' element={<LoginSignup/>}/>
                 </Routes>
+                <Footer/>
+                </BrowserRouter>
         </div>
     );
 }
---
+++ src/components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './NewsLetter.css'
+
+ function NewsLetter() {
+  return (
+    <div className='newsletter'>
+        <h1>Get Exclusive Offers on your Email</h1>
+        <p>Subscibe to our newletter and stay updated</p>
+    <div>
+    <input type="email" placeholder='Your Email id'/>
+    <button>Subscribe</button>
+    </div>
+    </div>
+  )
+}
+export default NewsLetter;
