df4b3697b02dace68e910e8d0660985a2961910d
Angad <angad04755@gmail.com>
2024-05-15 11:43:21 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import Navbar from "./components/Navbar/Navbar";
  import Product from './pages/Product';
  import LoginSignup from './pages/LoginSignup';
 import Footer from './components/Footer/Footer';
-
+import men_banner from './Assets/banner_mens.png';
+import women_banner from './Assets/banner_women.png';
+import kid_banner from './Assets/banner_kids.png';
 
 function App(){
     return(
@@ -15,9 +17,9 @@ function App(){
                 <Navbar/>
                 <Routes>
                     <Route path='/' element={<Shop/>}></Route>
-                    <Route path='/men' element={<ShopCategory category="men"/>}></Route>
-                    <Route path='/women' element={<ShopCategory category="women"/>}></Route>
-                    <Route path='/kids' element={<ShopCategory category="kids"/>}></Route>
+                    <Route path='/men' element={<ShopCategory banner={men_banner} category="men"/>}></Route>
+                    <Route path='/women' element={<ShopCategory banner={women_banner} category="women"/>}></Route>
+                    <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kids"/>}></Route>
 
 
                     <Route path="/product" element={<Product/>}>
