99cdf33df450dfd89f6564eaf35049839819e974
Angad <angad04755@gmail.com>
2024-05-26 16:48:50 +0530

    Added cart Algorithm

---
+++ src/CartItems/CartItems.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './CartItems.css'
+ function CartItems() {
+  return (
+    <div className='cartitems'></div>
+  )
+}
+export default CartItems;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import CartItems from '../CartItems/CartItems';
 
  function Cart() {
   return (
-    <div>Cart</div>
+    <div>
+      <CartItems/>
+    </div>
   )
 }
 export default Cart;
